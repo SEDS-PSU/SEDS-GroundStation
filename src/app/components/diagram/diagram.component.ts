bfa2891f4358dfeec9ccdb3fd2cc065555e5882c
joeHealy28 <89946568+joeHealy28@users.noreply.github.com>
2021-12-01 21:59:21 -0500

    Valve Sequencing Addition
    I changes some methods and added a few in "diagram" to test valve sequencing. One dummy sequence is implemented to test the code.
    
    The one sequence is usable with a button. When pressed, it closes all valves and opens the ones listed in the sequence.
    
    Moving forward, we could add each sequence in a drop-down menu or series of buttons. We could also add delays to these sequences. However, it may be necessary to add a separate component to organize all theses sequences on screen.

--- src/app/components/diagram/diagram.component.ts
+++ src/app/components/diagram/diagram.component.ts
@@ -10,39 +10,118 @@ export class DiagramComponent implements OnInit {
   constructor() {}
 
   map = new Map([
-    ['FO-P', 'closed'],
-    ['FO-FP', 'closed'],
-    ['FO-OP', 'closed'], // map of all our valves and their state
-    ['FC-OP', 'closed'],
-    ['FC-FP', 'closed'],
-    ['FC1-O', 'closed'],
-    ['FC2-O', 'closed'],
-    ['FO-O', 'closed'],
-    ['FC-P', 'closed'],
-    ['PV-F', 'closed'],
-    ['PV-O', 'closed'],
-    ['KILL', 'off']
+    ['FO-P', false],
+    ['FO-FP', false],
+    ['FO-OP', false], // map of all our valves and their state
+    ['FC-OP', false], // closed/off corresponds to false, open/on corresponds to true
+    ['FC-FP', false],
+    ['FC1-O', false],
+    ['FC2-O', false],
+    ['FO-O', false],
+    ['FC-P', false],
+    ['PV-F', false],
+    ['PV-O', false],
+    ['KILL', false]
   ]);
 
-  public toggleState(buttonNum) {
-    // method to toggle color of buttons
-    if(this.map.get('KILL') === 'off'){ // if kill switch is toggled we don't want to be able to change valve states
-      if (this.map.get(buttonNum) === 'closed') { // if the button pressed is closed set value to open and change color
-          document.getElementById(buttonNum).style.backgroundColor = '#32CD32';
-          this.map.set(buttonNum, 'open');
-      } else if (this.map.get(buttonNum) === 'open') {
-          document.getElementById(buttonNum).style.backgroundColor = 'red';
-          this.map.set(buttonNum, 'closed');
-      } else if (this.map.get(buttonNum) === 'off'){ // if kill switch is pressed, set value to 'on' and reset all valve values to off
-          for(var [key, value] of this.map.entries()){
-            document.getElementById(key).style.backgroundColor = 'red';
-            this.map.set(key, 'closed');
-          } // resets states to closed
-          document.getElementById(buttonNum).style.backgroundColor = 'black';
-          this.map.set(buttonNum, 'on');
+  resetSequence: [string, boolean][] = [
+    ['FO-P', false],
+    ['FO-FP', false],
+    ['FO-OP', false], // This is the base sequence where all valves are closed
+    ['FC-OP', false],
+    ['FC-FP', false],
+    ['FC1-O', false],
+    ['FC2-O', false],
+    ['FO-O', false],
+    ['FC-P', false],
+    ['PV-F', false],
+    ['PV-O', false],
+    ['KILL', false]
+  ];
+
+  /* each sequence is given a name, each valve to be open is listed
+  add each sequence once we know what they are.
+  */
+  sequenceList = new Map([
+    ['Sequence 1', [
+      'FO-P',
+      'FO-FP',
+      'FO-OP'
+    ]]
+  ]);
+
+  /* method to iterate through and update 
+    each valve when a sequence is selected
+  */
+  public changeValveSequence(sequenceNum) {
+
+    //valve sequence changes shouldn't happen when kill is active
+    if(this.map.get('KILL') === false)
+    {
+      //first, default all to closed states
+      for(let i = 0; i < this.resetSequence.length; i++) {
+        let valve = this.resetSequence[i][0];
+        let newState = this.resetSequence[i][1];
+
+        if(this.map.has(valve) && !(valve === 'KILL')) {
+          this.map.set(valve, newState);
+        }
+      }
+
+      //second, set valves to selected sequence
+      let sequence = this.sequenceList.get(sequenceNum);
+
+      for(let i = 0; i < sequence.length; i++) {
+        let valve = sequence[i];
+
+        if(this.map.has(valve)) {
+          this.map.set(valve, true); //sets the selected valve to open (true)
+        }
       }
     }
-  } // ends toggleColor
+
+    this.updateColor();
+  } // ends changeValveSequence
+
+  /* toggleState method changes the state of
+    each button between true and false, kill switch
+    will set all buttons to false
+  */
+  public toggleState(buttonNum) {
+
+    if(this.map.get('KILL') === false && buttonNum !== 'KILL'){ // if kill switch is toggled we don't want to be able to change valve states
+      if (this.map.get(buttonNum) === false) { // if the button pressed is closed set value to open and change color
+          this.map.set(buttonNum, true);
+      } 
+      else if (this.map.get(buttonNum) === true) {
+          this.map.set(buttonNum, false);
+      } 
+
+      this.updateColor();
+    }
+    else if (buttonNum === 'KILL') { // if kill switch is pressed, set value to 'on' and reset all valve values to off
+      for(var [key, value] of this.map.entries()) {
+        this.map.set(key, false);
+      } // resets states to closed
+
+      this.map.set(buttonNum, true);
+      this.updateColor();
+      document.getElementById(buttonNum).style.backgroundColor = 'black';
+    }
+
+  } // ends toggleState
+
+  public updateColor() {
+
+    for(var [key, value] of this.map.entries()) {
+      if(value === false && key !== 'KILL') {
+        document.getElementById(key).style.backgroundColor = 'red';
+      }
+      else if(value === true && key !== 'KILL') {
+        document.getElementById(key).style.backgroundColor = '#32CD32';
+      }
+    }
+  }
 
 /* below is the code for the killswitch timer
   It uses the builtin setInterval function to
@@ -59,7 +138,7 @@ export class DiagramComponent implements OnInit {
       console.log(this.timeLeft);
       if(this.timeLeft <= 1){
         console.log("HERE");
-        this.map.set('KILL', 'off');
+        this.map.set('KILL', false);
       }
       this.timeLeft = 5;
       clearInterval(this.interval);
